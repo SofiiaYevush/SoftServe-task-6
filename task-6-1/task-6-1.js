359e65c4451a8d2ac0160b28362838939d7a7e55
Sofiia13 <sofiia.yevush@gmail.com>
2023-11-02 14:27:26 +0200

    first commit

---
+++ task-6-1/task-6-1.js
@@ -0,0 +1,9 @@
+const list = document.getElementById('list'); 
+const listItems = list.querySelectorAll("li");
+const order = [0, 4, 1, 3, 2];
+let result = "";
+    for (let i = 0; i < order.length; i++) {
+        const index = order[i];
+        result += listItems[index].textContent + " ";
+    }
+alert(result);
